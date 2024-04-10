2a4a8be92b97199a40bda474a59fd5815d12cda4
kaola526 <huweixiong@storswift.com>
2024-04-10 18:43:24 +0800

    add flag data

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,21 @@ use structopt::StructOpt;
 struct Opt {
     #[structopt(short = "p", long = "port")]
     port: Option<u16>,
+
+    #[structopt(short = "d", long = "data")]
+    data: Option<String>,
 }
 
 
 fn main() {
     let opt = Opt::from_args();
 
-    let port = opt.port.unwrap_or(8080);
-
     let coinbase_puzzle = CoinbasePuzzle::<CurrentNetwork>::load().unwrap();
-    server::start_rpc_server(port, coinbase_puzzle);
+
+    if let Some(data) = opt.data {
+        server::check_aleo_proof(&data, coinbase_puzzle);    
+    } else {
+        let port = opt.port.unwrap_or(8080);
+        server::start_rpc_server(port, coinbase_puzzle);
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -132,6 +132,74 @@ pub fn start_rpc_server(port: u16, coinbase_puzzle: CoinbasePuzzle<CurrentNetwor
     server.wait();
 }
 
+pub fn check_aleo_proof(data: &str, coinbase_puzzle: CoinbasePuzzle<CurrentNetwork>) -> Result<JsonValue, ()> {
+    let parsed: Value = serde_json::from_str(data).unwrap();
+    let input_param = parsed.get("input_param").and_then(JsonValue::as_str).map(|s| s.to_string());
+    let input_proof = parsed.get("proof").and_then(JsonValue::as_str).map(|s| s.to_string());
+
+    if let (Some(input_param), Some(input_proof)) = (input_param, input_proof) {
+        let input_param: Value = serde_json::from_str(&input_param).unwrap();
+        let nonce_ex = input_param.get("nonce_ex").and_then(JsonValue::as_u64); 
+        let nonce_len = input_param.get("nonce_len").and_then(JsonValue::as_u64);
+        let target = input_param.get("min_proof_target").and_then(JsonValue::as_u64);
+        let address = input_param.get("address").and_then(JsonValue::as_str).map(|s| s.to_string());
+
+        if let (Some(nonce_ex), Some(nonce_len), Some(target), Some(address)) = (nonce_ex, nonce_len, target, address) {
+            let address = &address;
+            let displacement = (8u64 - nonce_len) << 3;
+            let nonce_ex = nonce_ex << displacement;
+            let mix_target = target;
+
+            if let Some((task_id, nonce, challenge,solution, proof, target)) = parse_parameters(&input_proof) {
+                let commitment = PuzzleCommitment::<CurrentNetwork>::from_str(&solution).unwrap();
+                let proof = PuzzleProof::<CurrentNetwork>::from_bytes_le(
+                    hex::decode(&proof).unwrap().as_slice(),
+                ).unwrap();
+
+                let address = Address::<CurrentNetwork>::from_str(&address).unwrap();
+                let prover_solution = ProverSolution::<CurrentNetwork>::new(
+                    PartialSolution::<CurrentNetwork>::new(address, nonce, commitment),
+                    proof,
+                );
+                let challenge = EpochChallenge::<CurrentNetwork>::from_bytes_le(
+                    hex::decode(&challenge).unwrap().as_slice(),
+                )
+                .unwrap();
+    
+                if (nonce_ex & nonce) != nonce_ex {
+                    println!("false");
+                    Ok(Value::Bool(false))
+                } else {
+                    if mix_target > target {
+                        println!("false");
+                        return Ok(Value::Bool(false))
+                    }
+                    if let Ok(verify) = prover_solution.verify(
+                        coinbase_puzzle.coinbase_verifying_key(),
+                        &challenge,
+                        target,
+                    ) {
+                        println!("{verify}");
+                        Ok(Value::Bool(verify))
+                    } else {
+                        println!("false");
+                        Ok(Value::Bool(false))
+                    }
+                }
+            } else {
+                println!("false");
+                Ok(Value::Bool(false))
+            }
+        } else {
+            println!("false");
+            Ok(Value::Bool(false))
+        }
+    } else {
+        println!("false");
+        Ok(Value::Bool(false))
+    }
+}
+
 fn parse_parameters(input: &str) -> Option<(u64, u64, String, String, String, u64)> {
     let mut task_id: Option<u64> = None;
     let mut nonce: Option<u64> = None;
